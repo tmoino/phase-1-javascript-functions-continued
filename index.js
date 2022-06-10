9bbcd6dda1190839ccabb58055a897709abe8415
tmoino <tony.moino@student.moringaschool.com>
2022-06-10 15:26:28 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun (act= "roller-skate"){  
+        return (`This Saturday, I want to ${act}!`);   
+    }
+   
+function mondayWork(act ="go to the office"){
+    return `This Monday, I will ${act}.`
+    }
+
+function wrapAdjective (sty = "*"){
+    return function (adj= "a hard worker"){
+        return `You are ${sty}${adj}${sty}!`
+}
+}
+function wrapAdjective (sty = "||"){
+    return function (adj = "a dedicated programmer"){
+        return `You are ${sty}${adj}${sty}!`
+    }
+
+}
+
